aeb1cd27196e109a84c026b5cd6fa4f163b0db66
Timmy <timmy@choco.media>
2021-05-08 21:31:22 +0800

    Initial project.

---
+++ app/src/main/java/com/timmy/codelab/customview/LobbyActivity.kt
@@ -0,0 +1,11 @@
+package com.timmy.codelab.customview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LobbyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby)
+    }
+}
